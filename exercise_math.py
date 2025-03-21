856762ab67767e271a81b3a58c9f03d581b8d55a
pdeluchi <pdeluchi@mail.austral.edu.ar>
2025-03-21 10:41:10 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,13 +1,10 @@
 def math():
     a = 57
     b = 7
-    print("valor de a:")
-    print (a)
-    print("valor de b:")
-    print(b)
-    print (a+b)
-    print (a-b)
-    print (a*b)
-    print (int(a/b))
-    print (a%b)
-    print (a/b)
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print((a+b)/2)
+    print(int(a/b))
+    print(a%b)
+    print(a/b)
