0c4615574801f59028c7b202714a06fa18658a24
pdeluchi <pdeluchi@mail.austral.edu.ar>
2025-03-21 10:37:58 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,13 +1,13 @@
 def math():
     a = 57
     b = 7
-print("valor de a:")
-print (a)
-print("valor de b:")
-print(b)
-print (a+b)
-print (a-b)
-print (a*b)
-print (int(a/b))
-print (a%b)
-print (a/b)
+    print("valor de a:")
+    print (a)
+    print("valor de b:")
+    print(b)
+    print (a+b)
+    print (a-b)
+    print (a*b)
+    print (int(a/b))
+    print (a%b)
+    print (a/b)
